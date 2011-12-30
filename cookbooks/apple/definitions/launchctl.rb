ccf7dbd1a9518ac3ff8066ab0067f9d1b86764ca
Michael Schubert <michael@schubert.cx>
2011-12-30 00:47:44 -0500

    WIP

--- cookbooks/apple/definitions/launchctl.rb
+++ cookbooks/apple/definitions/launchctl.rb
@@ -2,11 +2,14 @@ define :launchctl, :action => :load do
   user_to_execute_as = params[:user]  ? params[:user] : ENV["USER"]
   action = params[:action].to_s
   raise "Unknown action #{action}" unless ["load", "unload"].include?(action)
-  
+  not_if_conditions = params[:not_if] || lambda { false }
+  only_if_conditions = params[:only_if] || lambda { true }
   options = ""
   options << "-w" if File.exist?(params[:name])
-  
+
   execute "launchctl #{action} #{options} #{params[:name]}" do
+    not_if not_if_conditions
+    only_if only_if_conditions
     command "launchctl #{action} #{options} #{params[:name]}"
     user user_to_execute_as
   end
